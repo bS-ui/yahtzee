8c6766a007512b4416abcb4db7130ea35cf02e1a
brandonS <brandonxsears@gmail.com>
2024-03-28 15:32:08 -0400

    Adds ability to move dice from display to keepers and visa versa, styles board a bit

--- js/app.js
+++ js/app.js
@@ -1,12 +1,71 @@
 let userRoll;
+let keepers = [];
+let rollCount = 0
+
+//r ===============================================================================
 
 const diceDisplayEl = document.querySelectorAll('.dice-display')
+const keeperDisplayEl = document.querySelectorAll('.keeper-dice')
+const rollButtonEl = document.querySelector('.roll-button')
+const rollCountEl = document.querySelector('.roll-count')
 
-console.log(diceDisplayEl)
-doRoll = () => {
-  userRoll.forEach((roll,idx) => userRoll[idx] = Math.floor(Math.random() * 5))
+//o ===============================================================================
+
+diceDisplayEl.forEach(diceDisplay => {
+  diceDisplay.addEventListener('click', handleDiceDisplayClick)
+})
+
+keeperDisplayEl.forEach(keeperDisplay => {
+  keeperDisplay.addEventListener('click', handleKeeperDisplayClick)
+})
+
+rollButtonEl.addEventListener('click', handleRollBtnClick)
+
+//y ===============================================================================
+
+function handleDiceDisplayClick(evt) {
+  let clickIdx = evt.target.id.substring(1)
+  if (userRoll[clickIdx] !== null) {
+    keepers[clickIdx] = Number((userRoll.splice(clickIdx,1,null)))
+    console.log(userRoll)
+    console.log(keepers)
+    updateDisplays()
+  }
 }
-updateRollElement = () => {
+
+function handleKeeperDisplayClick(evt) {
+  let clickIdx = evt.target.id.substring(1)
+  if (keepers[clickIdx] !== null) {
+    userRoll[clickIdx] = Number((keepers.splice(clickIdx,1,null)))
+    console.log(userRoll)
+    console.log(keepers)
+    updateDisplays()
+  }
+}
+
+function handleRollBtnClick() {
+  if (rollCount !== 3) {
+    doRoll()
+    console.log(userRoll)
+    updateDisplays()
+  }
+}
+
+//g ===============================================================================
+
+doRoll = () => {
+  userRoll.forEach((roll,idx) => {
+    if (rollCount > 0 && userRoll[idx] !== null) {
+      userRoll[idx] = Math.floor(Math.random() * 5)
+    }
+    else if (rollCount === 0) {
+      userRoll[idx] = Math.floor(Math.random() * 5)
+    }
+  })
+  rollCount += 1
+}
+
+updateDisplays = () => {
   diceDisplayEl.forEach((diceDisplay,idx) => {
     if (userRoll[idx] == '0') diceDisplay.style.backgroundImage = "url('./assets/images/dice1.png')"
     else if (userRoll[idx] == '1') diceDisplay.style.backgroundImage = "url('./assets/images/dice2.png')"
@@ -14,12 +73,25 @@ updateRollElement = () => {
     else if (userRoll[idx] == '3') diceDisplay.style.backgroundImage = "url('./assets/images/dice4.png')"
     else if (userRoll[idx] == '4') diceDisplay.style.backgroundImage = "url('./assets/images/dice5.png')"
     else if (userRoll[idx] == '5') diceDisplay.style.backgroundImage = "url('./assets/images/dice6.png')"
+    else diceDisplay.style.backgroundImage = 'none'
   })
-}
-init = () => {
-  userRoll = [null,null,null,null,null]
+  keeperDisplayEl.forEach((keeperDisplay,idx) => {
+    if (keepers[idx] == '0') keeperDisplay.style.backgroundImage = "url('./assets/images/dice1.png')"
+    else if (keepers[idx] == '1') keeperDisplay.style.backgroundImage = "url('./assets/images/dice2.png')"
+    else if (keepers[idx] == '2') keeperDisplay.style.backgroundImage = "url('./assets/images/dice3.png')"
+    else if (keepers[idx] == '3') keeperDisplay.style.backgroundImage = "url('./assets/images/dice4.png')"
+    else if (keepers[idx] == '4') keeperDisplay.style.backgroundImage = "url('./assets/images/dice5.png')"
+    else if (keepers[idx] == '5') keeperDisplay.style.backgroundImage = "url('./assets/images/dice6.png')"
+    else keeperDisplay.style.backgroundImage = 'none'
+  })
+  rollCountEl.textContent = `Roll Count: ${rollCount}`
 }
 
+init = () => {
+  userRoll = [null,null,null,null,null]
+  rollCountEl.textContent = `Roll Count: ${rollCount}`
+}
+
+//b ===============================================================================
+
 init()
-doRoll()
-updateRollElement()
